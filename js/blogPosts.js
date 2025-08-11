a8f711c3dbde8a85158f5f4abc058cc6759c4db6
joshberk <joshuaberkoh19@gmail.com>
2025-08-11 11:53:11 -0400

    added week 2 blog and changes in affiliate pages

--- js/blogPosts.js
+++ js/blogPosts.js
@@ -1,4 +1,13 @@
 const blogPosts = [
+
+  {
+    title: "Week 2 - The Birth of Modern Security",
+    url: "blog/cryptography/week-2-the_birth_of_modern_day_security.html",
+    date: "2025-08-11",
+    discipline: "Cryptography",
+    description: "My second week's dive into cryptography, exploring the transition from classical ciphers to modern security. This post unpacks Kerckhoffs's Principle, attack models, and why mathematical rigor is essential.",
+    type: "Study Notes"
+  },
   {
     title: "Week 1: Foundational Encryption Concepts",
     url: "blog/cryptography/week-1-foundational-encryption-concepts.html",
